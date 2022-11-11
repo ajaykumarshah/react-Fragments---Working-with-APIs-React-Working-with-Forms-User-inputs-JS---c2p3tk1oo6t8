38dd7b1f0f23bdc127bfb60785c22f5264542e0d
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-11 18:38:50 +0530

    h

--- src/components/App.js
+++ src/components/App.js
@@ -4,32 +4,39 @@ import '../styles/App.css';
 import List from "./List";
 
 const App = () => {
- 
-  const [value, setValue] = useState(0);
+
+  const [value, setValue] = useState("");
   const [list, setList] = useState([]);
 
-
+  let list2 = 0;
   const onButtonClick = () => {
-    setList([])
-    for (let i = 1; i < Number(value)+1; i++) {
-      
-      setList((prev) => [...prev, i]);
-    }
-   
-  };
- 
+    //   setList([])
+    //   for (let i = 1; i < Number(value) + 1; i++) {
 
+    //     setList((prev) => [...prev, i]);
+    //   }
+
+    // };
+    list.push(value);
+    list2 = list.map((element, index) => {
+      return <li className="items" key={element + index} >Hello the value is {index + 1}</li>
+    })
+    setList(list2);
+  }
   const onInputChange = (e) => {
+    e.persist();
     setValue(e.target.value);
+    // setList([...list, value]);
   };
 
   return (
     <div id="main">
 
-      <input id="input" onChange={onInputChange} />
+      <input id="input" value={value} onChange={onInputChange} />
       <button id="button" onClick={onButtonClick}>Click</button>
-       <ul id="list">
-      <List listx={list} />
+      <ul id="list">
+        {list}
+        {/* <List listx={list} /> */}
       </ul>
     </div>
   );
